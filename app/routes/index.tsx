5ab455a5ca0e78c794fbe85469161a352c6fd19a
Conal Cosgrove <conal.cosgrove@salesforce.com>
2022-03-18 23:54:34 +0000

    redirect from index to /home

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,9 @@
 import { redirect } from "remix";
+import type { LoaderFunction } from "remix";
 
+export const loader: LoaderFunction = () => {
+    return redirect("/home");
+}
 export default function Index() {
     return <div></div>;
 }
